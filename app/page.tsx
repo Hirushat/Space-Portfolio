b5aa7387a616c91455743a0a72e252df706c0c58
Hirushat <hirusha10online@gmail.com>
2024-06-01 22:02:26 +0530

    Added Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Encryption from "@/components/main/Encryption";
+import Footer from "@/components/main/Footer";
 import Hero from "@/components/main/Hero";
 import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
@@ -7,7 +8,7 @@ import Image from "next/image";
 export default function Home() {
   return (
  <main className='h-full w-full'>
-  <div className="flex flex-col h-[850px] gap-20">
+  <div className="flex flex-col h-full gap-20">
       <Hero/>
       <Skills/>
       <Encryption/>
